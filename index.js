52aaa07b5832cca34be06ed399e539721fc75fca
Ryan Lee <rjlguitar@gmail.com>
2021-02-18 14:35:43 -0800

    added weatherData

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ async function getWeather(apikey, zip) {
   try {
     const res = await fetch(path);
     const json = await res.json();
-
-    return json;
+    console.log(json);
+    const weatherData = {
+      code: json.cod,
+      coord: json.coord,
+      temp: json.main.temp,
+      feels_like: json.main.feels_like,
+      temp_min: json.main.temp_min,
+      temp_max: json.main.temp_max,
+      description: json.weather[0].description,
+    };
+    return weatherData;
   } catch (err) {
     return err;
   }
